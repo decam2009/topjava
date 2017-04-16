8c26a4653aca395c9f0c06f806a9eb3dd5d1e9fd
Boris Kaloshin <decam2009@gmail.com>
2017-04-16 15:39:39 +0500

    HW0 Done

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -17,7 +17,8 @@ import java.util.stream.Collectors;
  * 31.05.2015.
  */
 public class UserMealsUtil {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+	{
         List<UserMeal> mealList = Arrays.asList(
                 new UserMeal(LocalDateTime.of(2017, Month.MAY, 30,10,0), "Завтрак", 1500),
                 new UserMeal(LocalDateTime.of(2017, Month.MAY, 30,13,0), "Обед", 600),
@@ -32,7 +33,8 @@ public class UserMealsUtil {
 //        .toLocalTime();
     }
 
-    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay)
+	{
 	  Map<LocalDate, Integer> caloriesSumByDate = mealList.stream()
 			  .collect(Collectors.groupingBy(um -> um.getDateTime().toLocalDate(), Collectors.summingInt(UserMeal::getCalories)));
 	  return mealList.stream()
